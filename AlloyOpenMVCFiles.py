3ea63c7000e46bf3fb52b6214e146a477f8ca13b
rudzon <rudzon@gmail.com>
2015-08-11 19:54:43 +0300

    now opens MVC view from any view controller or tss added commands to switch between views
    [
       { "keys": ["super+shift+c"], "command": "ti_switch", "args": {"type": "controller"} },
       { "keys": ["super+shift+v"], "command": "ti_switch", "args": {"type": "view"} },
       { "keys": ["super+shift+s"], "command": "ti_switch", "args": {"type": "style"} }
    ]

--- AlloyOpenMVCFiles.py
+++ AlloyOpenMVCFiles.py
@@ -1,4 +1,28 @@
-import sublime, sublime_plugin
+import sublime, sublime_plugin, re, os, functools
+
+TYPES = { 
+			"controller" : { 
+				"folder" : "/controllers/",
+				"ext" : ".js",
+				"group" : 0
+			},
+			"view" : { 
+				"folder" : "/views/",
+				"ext" : ".xml",
+				"group" : 2
+			},
+			"style" : {
+				"folder" : "/styles/",
+				"ext" : ".tss",
+				"group" : 1
+			}
+		}
+
+def translateToType(fullpath, command):
+		dirName = os.path.dirname(fullpath) + "/"
+		baseName, extension = os.path.splitext(os.path.basename(fullpath))
+		newDirName = functools.reduce(lambda acc, x: acc.replace( x["folder"], TYPES[command]["folder"]), TYPES.values(), dirName)
+		return newDirName + baseName + TYPES[command]["ext"]
 
 def fixed_set_layout(window, layout):
 	#A bug was introduced in Sublime Text 3, sometime before 3053, in that it
@@ -18,9 +42,27 @@ class NewWindowWithAlloyFilesCommand(sublime_plugin.WindowCommand):
 		sublime.active_window().run_command("new_window")
 		new_window = sublime.active_window()
 		fixed_set_layout(new_window, layout)
-		new_window.focus_group(2)
-		new_window.open_file(fname.replace("/controllers/", "/views/").replace(".js", ".xml"))
-		new_window.focus_group(1)
-		new_window.open_file(fname.replace("/controllers/", "/styles/").replace(".js", ".tss"))				
-		new_window.focus_group(0)
-		new_window.open_file(fname)		
+		
+		for key, value in TYPES.items():
+			new_window.focus_group(value["group"])
+			new_window.open_file(translateToType(fname, key))
+
+
+class TiSwitchCommand(sublime_plugin.WindowCommand):
+	def run(self, *args, **kwargs):
+		activeWindow = sublime.active_window();
+		activeView = activeWindow.active_view()
+		currentFile = activeView.file_name()
+		switchType = kwargs["type"];
+		fileSwitchTo = translateToType(currentFile, switchType)
+		print(currentFile)
+		print(fileSwitchTo)
+
+		if not os.path.exists(fileSwitchTo) :
+			print("File does not exist");
+			return
+
+		if activeWindow.num_groups() == len(TYPES) :
+			activeWindow.focus_group(TYPES[switchType]["group"])
+		
+		activeWindow.open_file(fileSwitchTo)
